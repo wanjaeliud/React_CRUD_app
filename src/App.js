7dcf514465a2b4bb4837864f3502e0c29d10dbeb
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-07-29 17:04:37 +0300

    Add table to hold data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
 import AddTutorial from './Components/AddTutorial';
+import AllTutorials from './Components/AllTutorials';
 
 const App = () => {
   return (
       <Router>
         <Navbar />
         <Home />
+        <AllTutorials/>
         <AddTutorial />
       </Router>
 
---
+++ src/Components/AllTutorials.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  makeStyles,
+  Table, TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+
+const useStyle = makeStyles({
+  table: {
+    width: '80%',
+    margin: '50px 100px 100px 140px',
+  },
+  thead: {
+    '& > *': {
+      background: '#000000',
+      color: '#FFFFFF',
+      fontSize: '16px',
+    },
+  },
+  trow: {
+    '& > *': {
+      fontSize: '16px',
+    },
+  },
+});
+
+const AllTutorials = () => {
+  const classes =useStyle()
+  return (
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow className={classes.thead}>
+            <TableCell>ID</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Author</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+  );
+};
+
+export default AllTutorials;
