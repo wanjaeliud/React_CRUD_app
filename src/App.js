a77c48f79bf42e4e1de8cbe4df2122256f338c28
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-07-29 17:41:38 +0300

    page routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+// import React from 'react';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
 import AddTutorial from './Components/AddTutorial';
 import AllTutorials from './Components/AllTutorials';
 import EditTutorial from './Components/EditTutorial';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
   return (
       <Router>
         <Navbar />
-        <AddTutorial />
-        <Home />
-        <AllTutorials/>
-        <EditTutorial/>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/all" component={AllTutorials} exact />
+          <Route path="/add" component={AddTutorial} exact />
+          <Route path="/edit/:id" component={EditTutorial} exact />
+          {/*<Route component={NotFound} />*/}
+        </Switch>
 
       </Router>
 
